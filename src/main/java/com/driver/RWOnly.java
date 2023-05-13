90b277ed5814151cebab2e51a7ba620e97ae7747
Neelam Dubey <ashidubey8208@gmail.com>
2023-05-13 17:20:04 +0530

    files added

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+    private int age;
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
 }
